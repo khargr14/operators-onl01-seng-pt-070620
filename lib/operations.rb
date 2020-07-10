13f005bd19d6fc17cfbef572a4f8586a3fe7d91d
Keesha Hargrove <kharg14@gmail.com>
2020-07-10 00:33:07 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,26 @@
-def unsafe?(speed)
 
-end
+def unsafe?(79)
+	 if speed > 60
+    true
+  elsif speed < 40
+    true
+  else
+    false
+  end
 
 
 
-def not_safe?(speed)
 	
-end
-	
+def unsafe?(79)
+	if speed < 40 || speed > 60
+		true
+	else
+		false
+	end
+  
+ 
+
+
+
 
 
